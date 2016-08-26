2f42882c7386ad11ca2d06b14e43607fb66aa44c
Bharat Shrinevas <shrinba@yahoo.com>
2016-08-26 14:49:19 -0700

    add to_region

--- cleaning.py
+++ cleaning.py
@@ -50,3 +50,27 @@ class Cleaner(object):
 
         # Drop the none or unspecified column
         self.df.drop('None or Unspecified', axis =1)
+
+    def add_region(self):
+        self.northeast = ('New York','Pennsylvania','New Jersey','Connecticut','Rhode Island','Massachusetts','New Hampshire','Vermont','Maine')
+        self.southeast = ('Arkansas','Louisiana','Mississippi','Alabama','Tennessee','Kentucky','Georgia','North Carolina','South Carolina','Virginia','West Virginia','Maryland','Delaware','Washington DC', 'Florida')
+        self.midwest = ('North Dakota','South Dakota','Nebraska','Kansas','Minnesota','Iowa','Missouri','Wisconsin','Illinois','Michigan','Ohio','Indiana')
+        self.west = ('Montana','Idaho','Wyoming','Colorado','Utah','Oregon','Washington', 'California','Nevada')
+        self.southwest = ('Texas','Arizona','New Mexico','Oklahoma')
+        self.other = ('Unspecified','Puerto Rico','Hawaii','Alaska')
+
+        self.df['region'] = self.df_in['state'].apply(_to_region)
+
+    def _to_region(self,state):
+        if state in self.northeast:
+            return 0
+        elif state in self.southeast:
+            return 1
+        elif state in self.midwest:
+            return 2
+        elif state in self.west:
+            return 3
+        elif state in self.southwest:
+            return 4
+        else:
+            return 5
